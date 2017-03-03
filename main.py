a2d4c06eac3b9f83b096adc84b99b74ce9e7e358
mari <maristein3@gmail.com>
2017-03-03 22:58:18 +0100

    all done BESIDES toggle which needs to work more than one time and documentation which is missing

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from models.todo import Todo
 import os
 
-
 app = Flask(__name__)
 
 app.config.update(dict(
     SECRET_KEY='sosecret',
-    DATABASE=os.path.join(app.root_path, 'db.sqlite'),
+    DATABASE=os.path.join(app.root_path, 'hello.db'),
     SITE_NAME='TO-DO App'
 ))
+
+Todo.create_database(os.path.join(app.root_path, 'hello.db'))
 
 
 @app.route("/")
 def list():
     """ Shows list of todo items stored in the database.
     """
-    all_tasks = Todo.get_all()
+    all_tasks = Todo.get_all(os.path.join(app.root_path, 'hello.db'))
     return render_template('index.html', all_tasks=all_tasks)
 
 
@@ -28,18 +29,18 @@ def add():
     """
     if request.method == 'POST':
         name = request.form['task']
-        task = Todo(0, name)
-        task.save()
+        task = Todo(None, name)
+        task.save(os.path.join(app.root_path, 'hello.db'))
         return list()
-    return render_template('new.html')
+    return render_template('add.html')
 
 
 @app.route("/remove/<todo_id>")
 def remove(todo_id):
     """ Removes todo item with selected id from the database """
 
-    task = Todo.get_by_id(todo_id)
-    task.delete()
+    task = Todo.get_by_id(todo_id, os.path.join(app.root_path, 'hello.db'))
+    task.delete(os.path.join(app.root_path, 'hello.db'))
     return list()
 
 
@@ -49,13 +50,26 @@ def edit(todo_id):
     If the method was GET it should show todo item form.
     If the method was POST it should update todo item in database.
     """
-    return "Edit " + todo_id
+    if request.method == 'POST':
+        old_task = Todo.get_by_id(todo_id, os.path.join(app.root_path, 'hello.db'))
+        name = request.form['name']
+        task = Todo(old_task.id, name)
+        task.update(os.path.join(app.root_path, 'hello.db'))
+        return list()
+    elif request.method == 'GET':
+        obj = Todo.get_by_id(todo_id, os.path.join(app.root_path, 'hello.db'))
+        old_name = obj.name
+        return render_template('update.html', old_name=old_name)
 
 
 @app.route("/toggle/<todo_id>")
 def toggle(todo_id):
     """ Toggles the state of todo item """
-    return "Toggle " + todo_id
+    task = Todo.get_by_id(todo_id, os.path.join(app.root_path, 'hello.db'))
+    task.toggle_object()
+    task.toggle(os.path.join(app.root_path, 'hello.db'))
+    return list()
 
 if __name__ == "__main__":
     app.run()
+
--- models/todo.py
+++ models/todo.py
@@ -1,47 +1,62 @@
 import sqlite3
-import os
-import os.path
 
 
 class Todo:
     """ Class representing todo item."""
 
-    def __init__(self, id, name, done=False):
+    def __init__(self, id, name, done=0):
         self.id = id
         self.name = name
         self.done = done
 
-    def toggle(self):
-        """ Toggles item's state """
-        self.done = True
+    def toggle_object(self):
+        """ Toggles item's state  """
+        if self.done == 0:
+            self.done = 1
+        else:
+            self.done = 0
+
+    def toggle(self, database_path):
+        """ Toggles item's state in database """
+        conn = sqlite3.connect(database_path)
+        c = conn.cursor()
+        c.execute("UPDATE tasks SET finished='{}' WHERE id = '{}'".format(self.done, self.id))
+        conn.commit()
+        conn.close()
 
     @staticmethod
     def create_database(database_path):
+        # package_dir = os.path.abspath(os.path.dirname("/home/mar/Documents/python-flask-todo-marmarmar/models"))
+        # database_path = os.path.join(package_dir, 'hello.db')
         conn = sqlite3.connect(database_path)
         c = conn.cursor()
         c.execute('''DROP TABLE IF EXISTS tasks;''')
         c.execute('''CREATE TABLE tasks
                      (id integer primary key autoincrement,
                      task text not null,
-                     finished boolean not null);''')
+                     finished integer);''')
         c.execute('''INSERT INTO tasks(id, task, finished) VALUES (null, "read", 0);''')
         conn.commit()
         conn.close()
 
-    def save(self):
-        """ Saves/updates todo item in database """
-        package_dir = os.path.abspath(os.path.dirname("/home/mar/Documents/python-flask-todo-marmarmar/models"))
-        database_path = os.path.join(package_dir, 'hello.db')
+    def save(self, database_path):
+        """ Saves todo item in database """
         conn = sqlite3.connect(database_path)
         c = conn.cursor()
         c.execute('''INSERT INTO tasks(task, finished) VALUES ("{}", "{}");'''.format(self.name, self.done))
         conn.commit()
         conn.close()
 
-    def delete(self):
+    def update(self, database_path):
+        """ Updates todo item in database """
+        conn = sqlite3.connect(database_path)
+        c = conn.cursor()
+        c.execute("UPDATE  tasks SET task = '{}'  WHERE id = '{}'".format(self.name, self.id))
+        conn.commit()
+        conn.close()
+
+    def delete(self, database_path):
         """ Removes todo item from the database """
-        package_dir = os.path.abspath(os.path.dirname("/home/mar/Documents/python-flask-todo-marmarmar/models"))
-        database_path = os.path.join(package_dir, 'hello.db')
         conn = sqlite3.connect(database_path)
         c = conn.cursor()
         c.execute('''DELETE FROM tasks WHERE id = {};'''.format(self.id))
@@ -49,16 +64,14 @@ class Todo:
         conn.close()
 
     @classmethod
-    def get_all(cls):
+    def get_all(cls, database_path):
         """ Retrieves all Todos form database and returns them as list.
         Returns:
             list(Todo): list of all todos
         """
-        package_dir = os.path.abspath(os.path.dirname("/home/mar/Documents/python-flask-todo-marmarmar/models"))
-        database_path = os.path.join(package_dir, 'hello.db')
         conn = sqlite3.connect(database_path)
         c = conn.cursor()
-        all_tasks = c.execute('''SELECT * FROM tasks;''')
+        all_tasks = c.execute('''SELECT * FROM tasks ORDER BY id;''')
         list_tasks = []
         for row in all_tasks:
             task = Todo(row[0], row[1], row[2])
@@ -67,15 +80,14 @@ class Todo:
         return list_tasks
 
     @classmethod
-    def get_by_id(cls, id):
+    def get_by_id(cls, id, database_path):
         """ Retrieves todo item with given id from database.
         Args:
             id(int): item id
+            database_path: os.path.join(app.root_path, 'hello.db')
         Returns:
             Todo: Todo object with a given id
         """
-        package_dir = os.path.abspath(os.path.dirname("/home/mar/Documents/python-flask-todo-marmarmar/models"))
-        database_path = os.path.join(package_dir, 'hello.db')
         conn = sqlite3.connect(database_path)
         c = conn.cursor()
         cursor_object = c.execute('''SELECT * FROM tasks WHERE id = {};'''.format(id))
@@ -84,10 +96,11 @@ class Todo:
         conn.close()
         return task_by_id
 
-
-def main():
-    # Todo.create_database(os.path.join(app.root_path, 'db.sqlite'))
-    print(Todo.get_all())
-    # print(Todo.get_by_id(1).name)
-
-main()
+#
+# def main():
+#     Todo.create_database(os.path.join(app.root_path, 'hello.db'))
+#     # print(Todo.get_all())
+#     # print(Todo.get_by_id(1).name)
+#
+#
+# main()
